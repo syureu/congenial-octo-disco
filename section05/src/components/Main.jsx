0c81a7486507344dc18cee623d9f00c2aae62d97
syureu <tor01237@gmail.com>
2024-08-07 19:57:51 +0900

    5.3

--- section05/src/components/Main.jsx
+++ section05/src/components/Main.jsx
@@ -9,7 +9,13 @@ const Main = () => {
     isLogin: true,
   };
 
-  return <>{user.isLogin ? <div>로그아웃</div> : <div>로그인</div>}</>;
+  if (user.isLogin) {
+    return <div>로그아웃</div>;
+  } else {
+    return <div>로그인</div>;
+  }
+
+  // return <>{user.isLogin ? <div>로그아웃</div> : <div>로그인</div>}</>;
 };
 
 export default Main;
