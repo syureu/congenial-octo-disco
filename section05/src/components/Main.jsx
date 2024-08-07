24164aff9548ed516a1b0b7f1d3e89b2b4429068
syureu <tor01237@gmail.com>
2024-08-07 19:52:23 +0900

    5.3

--- section05/src/components/Main.jsx
+++ section05/src/components/Main.jsx
@@ -1,10 +1,19 @@
+// JSX 주의 사항
+// 1. 중괄호 내부에는 자바스크립트 표현식만 넣을 수 있다
+// 2. 숫자, 문자열, 배열 값만 렌더링 된다
 const Main = () => {
-  const number = 10;
+  const number = 9;
 
   return (
     <main>
       <h1>main</h1>
       <h2>{number % 2 === 0 ? "짝수" : "홀수"}</h2>
+      {10}
+      {number}
+      {[1, 2, 3]}
+      {true}
+      {undefined}
+      {null}
     </main>
   );
 };
