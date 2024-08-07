4d129c256f71224aacb400255039b1530d7db051
syureu <tor01237@gmail.com>
2024-08-07 19:56:56 +0900

    5.3

--- section05/src/components/Main.jsx
+++ section05/src/components/Main.jsx
@@ -4,26 +4,12 @@
 // 3. 모든 태그는 닫혀있어야 한다
 // 4. 최상위 태그는 반드시 하나여야만 한다
 const Main = () => {
-  const number = 9;
-  const obj = { a: 1 };
+  const user = {
+    name: "이정환",
+    isLogin: true,
+  };
 
-  return (
-    // <div></div>
-    <>
-      {/* <img> */}
-      <img></img>
-      <h1>main</h1>
-      <h2>{number % 2 === 0 ? "짝수" : "홀수"}</h2>
-      {10}
-      {number}
-      {[1, 2, 3]}
-      {true}
-      {undefined}
-      {null}
-      {/* {obj} */}
-      {obj.a}
-    </>
-  );
+  return <>{user.isLogin ? <div>로그아웃</div> : <div>로그인</div>}</>;
 };
 
 export default Main;
