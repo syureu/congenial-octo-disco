846941b902c8fbffcc7aa30a8adfb359fd0a3c30
syureu <tor01237@gmail.com>
2024-08-20 19:21:01 +0900

    5.4

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = (props) => {
   console.log(props);
-  return <button>{props.text}</button>;
+  return <button style={{ color: props.color }}>{props.text}</button>;
 };
 
 export default Button;
