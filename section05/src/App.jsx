fd6289b3b1e038f887b2a84b1d64e73365ac4341
syureu <tor01237@gmail.com>
2024-08-20 19:18:16 +0900

    5.4

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,14 +1,12 @@
 import "./App.css";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
+import Button from "./components/Button";
 
 function App() {
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <Button />
+      <Button />
+      <Button />
     </>
   );
 }
