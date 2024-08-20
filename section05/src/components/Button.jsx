8b3a7f2a9e0ac6c3ca10a53097ca516305273a68
syureu <tor01237@gmail.com>
2024-08-20 19:23:06 +0900

    5.4

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -2,9 +2,13 @@ const Button = (props) => {
   console.log(props);
   return (
     <button style={{ color: props.color }}>
-      {props.text} - {props.color}
+      {props.text} - {props.color.toUpperCase()}
     </button>
   );
 };
 
 export default Button;
+
+Button.defaultProps = {
+  color: "black",
+};
