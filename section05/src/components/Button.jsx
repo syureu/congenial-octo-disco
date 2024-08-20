e00b28537e72154c71745791502ef359b4eed0bc
syureu <tor01237@gmail.com>
2024-08-20 19:39:57 +0900

    5.5

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,12 +1,14 @@
 const Button = ({ text, color = "black", children }) => {
-  const onClickButton = () => {
+  // 이벤트 객체
+  const onClickButton = (e) => {
+    console.log(e);
     console.log(text);
   };
 
   return (
     <button
       onClick={onClickButton}
-      onMouseEnter={onClickButton}
+      // onMouseEnter={onClickButton}
       style={{ color: color }}
     >
       {text} - {color.toUpperCase()}
