4ad4fe089cf45bf660c1f58e1147dfa4be083edc
syureu <tor01237@gmail.com>
2024-08-20 19:26:32 +0900

    5.4

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -2,9 +2,18 @@ import "./App.css";
 import Button from "./components/Button";
 
 function App() {
+  const buttonProps = {
+    text: "메일",
+    color: "red",
+    a: 1,
+    b: 2,
+    c: 3,
+  };
+
   return (
     <>
-      <Button text={"메일"} color={"red"} />
+      <Button text={"메일"} color={"red"} a={1} b={2} c={3} />
+      <Button {...buttonProps} />
       <Button text={"카페"} />
       <Button text={"블로그"} />
     </>
--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -8,6 +8,7 @@ const Button = ({ text, color }) => {
 
 export default Button;
 
+// react-dom_client.js?v=bf77cf82:519 Warning: Button: Support for defaultProps will be removed from function components in a future major release. Use JavaScript default parameters instead.
 Button.defaultProps = {
   color: "black",
 };
