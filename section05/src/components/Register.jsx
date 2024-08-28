97b4285468f35880a33febce5778e82a133b1e56
syureu <tor01237@gmail.com>
2024-08-28 18:49:06 +0900

    5.8

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -10,6 +10,7 @@ const Register = () => {
   const [name, setName] = useState("이름");
   const [birth, setBirth] = useState("");
   const [country, setCountry] = useState("");
+  const [bio, setBio] = useState("");
   const onChangeName = (e) => {
     setName(e.target.value);
   };
@@ -18,6 +19,9 @@ const Register = () => {
   };
   const onChangeCountry = (e) => {
     setCountry(e.target.value);
+  };
+  const onChangeBio = (e) => {
+    setBio(e.target.value);
   };
   return (
     <div>
@@ -34,7 +38,10 @@ const Register = () => {
           <option value="us">미국</option>
           <option value="uk">영국</option>
         </select>
-        {country}
+      </div>
+      <div>
+        <textarea value={bio} onChange={onChangeBio} />
+        {bio}
       </div>
     </div>
   );
