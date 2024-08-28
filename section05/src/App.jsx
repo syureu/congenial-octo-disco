e73a6d79bec44108ddf02dbaf2bf718f227e16f5
syureu <tor01237@gmail.com>
2024-08-28 18:09:33 +0900

    5.6

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -2,9 +2,20 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const state = useState(0);
-  console.log(state);
-  return <></>;
+  const [state, setState] = useState(0);
+
+  return (
+    <>
+      <h1>{state}</h1>
+      <button
+        onClick={() => {
+          setState(state + 1);
+        }}
+      >
+        +
+      </button>
+    </>
+  );
 }
 
 export default App;
