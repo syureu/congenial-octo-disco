6f942bc5afa08f67f3cb8e5b4eec935a44877f59
syureu <tor01237@gmail.com>
2024-08-28 18:16:27 +0900

    5.6

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -3,7 +3,8 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [light, setLight] = useState("OFF");
+  // const [light, setLight] = useState("OFF");
+  let light = "OFF";
 
   return (
     <>
@@ -11,7 +12,8 @@ function App() {
         <h1>{light}</h1>
         <button
           onClick={() => {
-            setLight(light === "ON" ? "OFF" : "ON");
+            // setLight(light === "ON" ? "OFF" : "ON");
+            light = light === "ON" ? "OFF" : "ON";
           }}
         >
           {light === "ON" ? "끄기" : "켜기"}
