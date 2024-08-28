92b1bf0a90f4cb108728c4a0922c3a3c9189a51b
syureu <tor01237@gmail.com>
2024-08-28 18:45:50 +0900

    5.8

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -9,11 +9,15 @@ import { useState } from "react";
 const Register = () => {
   const [name, setName] = useState("이름");
   const [birth, setBirth] = useState("");
+  const [country, setCountry] = useState("");
   const onChangeName = (e) => {
     setName(e.target.value);
   };
   const onChangeBirth = (e) => {
     setBirth(e.target.value);
+  };
+  const onChangeCountry = (e) => {
+    setCountry(e.target.value);
   };
   return (
     <div>
@@ -22,7 +26,15 @@ const Register = () => {
       </div>
       <div>
         <input value={birth} type="date" onChange={onChangeBirth} />
-        {birth}
+      </div>
+      <div>
+        <select value={country} onChange={onChangeCountry}>
+          <option></option>
+          <option>한국</option>
+          <option>미국</option>
+          <option>영국</option>
+        </select>
+        {country}
       </div>
     </div>
   );
