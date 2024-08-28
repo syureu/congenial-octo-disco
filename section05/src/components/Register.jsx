22639f525fc7d61c43a71f8a055bcc7deae3fa7e
syureu <tor01237@gmail.com>
2024-08-28 18:40:13 +0900

    5.8

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+
+// 간단한 회원가입 폼
+// 1. 이름
+// 2. 생년월일
+// 3. 국적
+// 4. 자기소개
+
 const Register = () => {
-  return <div>register</div>;
+  const [name, setName] = useState("이름");
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+  return (
+    <div>
+      <input value={name} onChange={onChangeName} placeholder={"이름"} />
+      {name}
+    </div>
+  );
 };
 
 export default Register;
