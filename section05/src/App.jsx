f91207c5b30cfa495a20d851ae091638eaf9bd7a
syureu <tor01237@gmail.com>
2024-08-28 18:35:08 +0900

    5.8

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,13 +1,10 @@
 import "./App.css";
-import { useState } from "react";
-import Bulb from "./components/Bulb";
-import Counter from "./components/Counter";
+import Register from "./components/Register";
 
 function App() {
   return (
     <>
-      <Bulb />
-      <Counter />
+      <Register />
     </>
   );
 }
