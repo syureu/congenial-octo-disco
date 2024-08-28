7a880cba971ade936912cc873def4cf0f89d4d34
syureu <tor01237@gmail.com>
2024-08-28 18:25:29 +0900

    5.7

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,19 +1,30 @@
 import "./App.css";
 import { useState } from "react";
 
+const Bulb = ({ light }) => {
+  console.log(light);
+  return (
+    <div>
+      {light === "ON" ? (
+        <h1 style={{ backgroundColor: "orange" }}>ON</h1>
+      ) : (
+        <h1 style={{ backgroundColor: "gray" }}>OFF</h1>
+      )}
+    </div>
+  );
+};
+
 function App() {
   const [count, setCount] = useState(0);
-  // const [light, setLight] = useState("OFF");
-  let light = "OFF";
+  const [light, setLight] = useState("OFF");
 
   return (
     <>
       <div>
-        <h1>{light}</h1>
+        <Bulb light={light} />
         <button
           onClick={() => {
-            // setLight(light === "ON" ? "OFF" : "ON");
-            light = light === "ON" ? "OFF" : "ON";
+            setLight(light === "ON" ? "OFF" : "ON");
           }}
         >
           {light === "ON" ? "끄기" : "켜기"}
