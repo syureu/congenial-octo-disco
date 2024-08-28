22451da051efcb35ccbe8aaec4645eb1963281c4
syureu <tor01237@gmail.com>
2024-08-28 18:07:17 +0900

    5.6

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,27 +1,10 @@
 import "./App.css";
-import Header from "./components/Header";
-import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  };
-
-  return (
-    <>
-      <Button text={"메일"} color={"red"} a={1} b={2} c={3} />
-      <Button {...buttonProps} />
-      <Button text={"카페"} />
-      <Button text={"블로그"}>
-        <div>자식요소</div>
-        <Header />
-      </Button>
-    </>
-  );
+  const state = useState();
+  console.log(state);
+  return <></>;
 }
 
 export default App;
