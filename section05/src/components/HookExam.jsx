d5238022d834f4f5ef367d0c3c0b5eeb9f73f0f6
syureu <tor01237@gmail.com>
2024-10-15 22:34:14 +0900

    5.11

--- section05/src/components/HookExam.jsx
+++ section05/src/components/HookExam.jsx
@@ -17,10 +17,12 @@ function useInput() {
 
 const HookExam = () => {
   const [input, onChange] = useInput();
+  const [input2, onChange2] = useInput();
 
   return (
     <div>
       <input value={input} onChange={onChange} />
+      <input value={input2} onChange={onChange2} />
     </div>
   );
 };
