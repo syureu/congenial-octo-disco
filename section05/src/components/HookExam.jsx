45ece9097bcdc8c4499a1046852d40202c9cf061
syureu <tor01237@gmail.com>
2024-10-15 22:23:23 +0900

    5.11

--- section05/src/components/HookExam.jsx
+++ section05/src/components/HookExam.jsx
@@ -1,3 +1,17 @@
+import { useState } from "react";
+
+// 3가지 hook 관련된 팁
+// 1. 함수 컴포넌트, 커스텀 훅 내부에서만 호출 가능
+
+const state = useState();
+/*
+Warning: Invalid hook call. Hooks can only be called inside of the body of a function component. This could happen for one of the following reasons:
+1. You might have mismatching versions of React and the renderer (such as React DOM)
+2. You might be breaking the Rules of Hooks
+3. You might have more than one copy of React in the same app
+See https://reactjs.org/link/invalid-hook-call for tips about how to debug and fix this problem.
+*/
+
 const HookExam = () => {
   return <div>hookexam</div>;
 };
