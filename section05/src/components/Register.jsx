b75f744bf79dd3ea86a9ed1a6f72db4341164f06
syureu <tor01237@gmail.com>
2024-10-15 22:13:04 +0900

    5.10

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -15,6 +15,7 @@ const Register = () => {
   });
 
   const countRef = useRef(0);
+  const inputRef = useRef();
 
   const onChange = (e) => {
     countRef.current++;
@@ -25,10 +26,18 @@ const Register = () => {
     });
   };
 
+  const onSubmit = () => {
+    if (!input.name) {
+      // 이름을 입력하는 DOM 요소 포커스
+      console.log(inputRef.current);
+    }
+  };
+
   return (
     <div>
       <div>
         <input
+          ref={inputRef}
           name="name"
           value={input.name}
           onChange={onChange}
@@ -54,6 +63,8 @@ const Register = () => {
       <div>
         <textarea name="bio" value={input.bio} onChange={onChange} />
       </div>
+
+      <button onClick={onSubmit}>제출</button>
     </div>
   );
 };
