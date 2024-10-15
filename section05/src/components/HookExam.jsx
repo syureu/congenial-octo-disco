47fd4bf7632e514d83d89ce142f589b1217058cc
syureu <tor01237@gmail.com>
2024-10-15 22:27:36 +0900

    5.11

--- section05/src/components/HookExam.jsx
+++ section05/src/components/HookExam.jsx
@@ -3,17 +3,20 @@ import { useState } from "react";
 // 3가지 hook 관련된 팁
 // 1. 함수 컴포넌트, 커스텀 훅 내부에서만 호출 가능
 // 2. 조건부로 호출될 수는 없다
+// 3. 나만의 훅(Custom Hook)을 직접 만들 수 있다.
 
 const HookExam = () => {
-  if (true) {
-    const state = useState();
-  }
+  const [input, setInput] = useState("");
 
-  for (let i = 0; i < 5; ++i) {
-    const state = useState();
-  }
+  const onChange = (e) => {
+    setInput(e.target.value);
+  };
 
-  return <div>hookexam</div>;
+  return (
+    <div>
+      <input value={input} onChange={onChange} />
+    </div>
+  );
 };
 
 export default HookExam;
