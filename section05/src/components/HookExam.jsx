cb9b007595b44279283b055edf9915c2a3c4a339
syureu <tor01237@gmail.com>
2024-10-15 22:26:09 +0900

    5.11

--- section05/src/components/HookExam.jsx
+++ section05/src/components/HookExam.jsx
@@ -2,9 +2,16 @@ import { useState } from "react";
 
 // 3가지 hook 관련된 팁
 // 1. 함수 컴포넌트, 커스텀 훅 내부에서만 호출 가능
+// 2. 조건부로 호출될 수는 없다
 
 const HookExam = () => {
-  const state = useState();
+  if (true) {
+    const state = useState();
+  }
+
+  for (let i = 0; i < 5; ++i) {
+    const state = useState();
+  }
 
   return <div>hookexam</div>;
 };
