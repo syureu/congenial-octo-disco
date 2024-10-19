cc830bd94eb24466312392e238d91403ed0251da
syureu <tor01237@gmail.com>
2024-10-19 22:25:21 +0900

    7.2

--- section07/src/App.jsx
+++ section07/src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
 
 function App() {
   let [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(`count: ${count}`);
+  }, [count]);
+  // 의존성 배열 (dependency array), deps
 
   const onClickButton = (controllNumber) => {
     setCount(count + controllNumber);
--- section07/src/components/Viewer.jsx
+++ section07/src/components/Viewer.jsx
@@ -1,5 +1,4 @@
 function Viewer({ count }) {
-  console.log(count);
   return (
     <div>
       <div>현재 카운트 : </div>
